02cf2b7c6445979bcfab3429b780938040cb4e89
vik-4994 <igor.gavrylenko05@gmail.com>
2020-09-17 20:02:15 +0300

    Сделал 4-тый модуль

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,5 +3,16 @@
  * @returns {string}
  */
 function camelize(str) {
-  // ваш код...
+  let arr = str.split("-");
+  let newArr = []
+  for (let i = 0; i < arr.length; i++) {
+    if (i > 0) {
+      newArr.push(arr[i][0].toUpperCase() + arr[i].slice(1))
+    }
+    else {
+      newArr.push(arr[i])
+    }
+  }
+  let result = newArr.join('');
+  return result;
 }
--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -5,5 +5,13 @@
  * @returns {string}
  */
 function showSalary(users, age) {
-  // ваш код...
+  let str = "";
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].age <= age && str === "") {
+      str = str + users[i].name + ", " + users[i].balance;
+    } else if (users[i].age <= age && str != "") {
+      str = str + "\n" + users[i].name + ", " + users[i].balance;
+    }
+  }
+  return str;
 }
